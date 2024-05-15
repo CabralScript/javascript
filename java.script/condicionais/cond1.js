a9e145e6f89232ffa6a71e19ae6c3b8104f0ebf1
Davi Cabral <davidasilvacabral315@gmail.com>
2024-05-15 16:08:44 -0300

    att

---
+++ java.script/condicionais/cond1.js
@@ -0,0 +1,23 @@
+//comparações
+// use (!== ára diferente de) e (=== para ver se os valores são identicos)
+
+const n1 = 7
+const n2 =8
+
+console.log(n1 !== n2)
+
+//conjunções lógicas
+const a = 18
+const b = 17
+console.log(b >= 18 || a >= 18)
+
+
+const l = 18
+const n = 17
+console.log (l >= 18 && n >= 18)
+
+console.log(!(n >= 18))
+//aqui ele está comparando se a pessoa é menor de idade
+
+//controle de fluxo condicionais
+/*paramos no 14;40*/ 
